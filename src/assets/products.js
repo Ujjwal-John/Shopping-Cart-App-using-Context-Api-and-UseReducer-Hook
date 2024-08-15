6e73c554bc66efc6dd532e2bf471fd1c4d8f5523
Ujjwal John <ujjwaljohn395@gmail.com>
2024-08-15 13:39:34 +0530

    Refactor product data to use static file and update configuration

---
+++ src/assets/products.js
@@ -0,0 +1,95 @@
+const products = [
+    {
+      id: '1',
+      name: 'Product 1',
+      price: '199.99',
+      image: 'https://via.placeholder.com/150',
+      inStock: 10,
+      fastDelivery: true,
+      ratings: 4
+    },
+    {
+      id: '2',
+      name: 'Product 2',
+      price: '299.99',
+      image: 'https://via.placeholder.com/150',
+      inStock: 5,
+      fastDelivery: false,
+      ratings: 3
+    },
+    {
+      id: '3',
+      name: 'Product 3',
+      price: '399.99',
+      image: 'https://via.placeholder.com/150',
+      inStock: 0,
+      fastDelivery: true,
+      ratings: 5
+    },
+    {
+      id: '4',
+      name: 'Product 4',
+      price: '149.99',
+      image: 'https://via.placeholder.com/150',
+      inStock: 8,
+      fastDelivery: false,
+      ratings: 2
+    },
+    {
+      id: '5',
+      name: 'Product 5',
+      price: '249.99',
+      image: 'https://via.placeholder.com/150',
+      inStock: 12,
+      fastDelivery: true,
+      ratings: 4
+    },
+    {
+      id: '6',
+      name: 'Product 6',
+      price: '349.99',
+      image: 'https://via.placeholder.com/150',
+      inStock: 6,
+      fastDelivery: false,
+      ratings: 5
+    },
+    {
+      id: '7',
+      name: 'Product 7',
+      price: '499.99',
+      image: 'https://via.placeholder.com/150',
+      inStock: 3,
+      fastDelivery: true,
+      ratings: 3
+    },
+    {
+      id: '8',
+      name: 'Product 8',
+      price: '599.99',
+      image: 'https://via.placeholder.com/150',
+      inStock: 2,
+      fastDelivery: true,
+      ratings: 4
+    },
+    {
+      id: '9',
+      name: 'Product 9',
+      price: '699.99',
+      image: 'https://via.placeholder.com/150',
+      inStock: 7,
+      fastDelivery: false,
+      ratings: 2
+    },
+    {
+      id: '10',
+      name: 'Product 10',
+      price: '799.99',
+      image: 'https://via.placeholder.com/150',
+      inStock: 4,
+      fastDelivery: true,
+      ratings: 5
+    }
+  ];
+  
+  export default products;
+  
--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -1,7 +1,9 @@
 import React, { useContext , useReducer } from 'react'
 import { createContext } from 'react'
-import {faker} from "@faker-js/faker"
+// import {faker} from "@faker-js/faker"
 import {cartReducer, productReducer} from "./Reducers"
+import products from '../assets/products'
+import PropTypes from 'prop-types';
 
 
 const Cart = createContext();
@@ -9,15 +11,17 @@ faker.seed(99);
 
 const Context = ({children}) => {
 
-    const products = [...Array(20)].map(() => ({
-        id: faker.string.uuid(),
-        name: faker.commerce.productName(),
-        price: faker.commerce.price(),
-        image: faker.image.url(),
-        inStock: faker.helpers.arrayElement([0, 3, 5, 6, 7]),
-        fastDelivery: faker.datatype.boolean(),
-        ratings: faker.helpers.arrayElement([1, 2, 3, 4, 5])
-      }));
+    // const products = React.useMemo(() =>{
+    //   return [...Array(20)].map(() => ({
+    //     id: faker.string.uuid(),
+    //     name: faker.commerce.productName(),
+    //     price: faker.commerce.price(),
+    //     image: faker.image.url(),
+    //     inStock: faker.helpers.arrayElement([0, 3, 5, 6, 7]),
+    //     fastDelivery: faker.datatype.boolean(),
+    //     ratings: faker.helpers.arrayElement([1, 2, 3, 4, 5])
+    //   }));
+    // },[]);
 
     const [state, dispatch] = useReducer(cartReducer, {
         products: products,
@@ -39,6 +43,10 @@ const Context = ({children}) => {
     </Cart.Provider>
   )
 }
+
+Context.propTypes = {
+  children: PropTypes.node.isRequired,
+};
 
 export default Context;
 
