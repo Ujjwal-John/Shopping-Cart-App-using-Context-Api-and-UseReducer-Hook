47ca89e296078394daac77fbaaca6e64d5eaaf80
Ujjwal John <ujjwaljohn395@gmail.com>
2024-08-15 14:22:36 +0530

    changes done

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "/",
+  base: "/Shopping-Cart-App-using-Context-Api-and-UseReducer-Hook/",
   plugins: [react()],
   build: {
     sourcemap: false,  // Enable source maps
