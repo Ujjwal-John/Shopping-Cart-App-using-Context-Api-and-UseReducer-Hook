d00559a183a91e67c58f39cdf7cd5ed2891c73ab
Ujjwal John <ujjwaljohn395@gmail.com>
2024-08-15 13:41:25 +0530

    Updates

---
+++ assets/Rating-CQByBxFw.js
@@ -0,0 +1,1 @@
+import{j as r}from"./react-JHr-MqlU.js";import{a as i,b as o}from"./react-icons-CUgsyQeK.js";const m=({rating:a,onClick:n,style:s})=>r.jsx(r.Fragment,{children:[...Array(5)].map((e,t)=>r.jsx("span",{onClick:()=>n(t),style:s,children:a>t?r.jsx(i,{fontSize:"15px"}):r.jsx(o,{fontSize:"15px "})},t))});export{m as R};
